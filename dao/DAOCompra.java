506500032d09ee967d9c33e28a4c133c1740ac0e
diegopa <diego.loge@hotmail.com>
2021-02-13 23:10:19 -0600

    Iniciando DAOCompra

---
+++ dao/DAOCompra.java
@@ -0,0 +1,11 @@
+/**
+ * Clase que conecta con la BD para los métodos de compras de insumos.
+ * @author Diego Puebla Aldama
+ * @version 1.0
+ */
+
+package ProyectoOpalo.dao;
+
+public class DAOCompra{
+
+}
