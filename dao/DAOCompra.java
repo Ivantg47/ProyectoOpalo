f8dc6e5e5805ba8f68dd369838d7a906cc600b07
diegopa <diego.loge@hotmail.com>
2021-02-14 00:56:24 -0600

    Agregando imports DAOCompra

--- dao/DAOCompra.java
+++ dao/DAOCompra.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import ProyectoOpalo.dto.DTOProducto;
+import ProyectoOpalo.dto.DTOCompra;
 import javax.swing.*;
 import javax.swing.table.*;
 
