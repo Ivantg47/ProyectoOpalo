65e00e0addb53173c5db10caafda84fafa689c28
ximenarf <pinkfull_xime@hotmail.com>
2021-02-15 02:19:45 -0600

    modificacion IGU y dao clientes

--- dao/DAOClientes.java
+++ dao/DAOClientes.java
@@ -60,8 +60,246 @@ public class DAOClientes{
 
 	public void agregarCliente(DTOClientes oCliente){
 
+		int eExecucion;
+		String buscar;
+		String insertar;
+		ResultSet oResultado;
 
+		try{
+
+			conexion = getConexion() ;
+
+			buscar =  	"SELECT id_cliente " +
+          				"FROM Cliente " 
+                         		+ "WHERE nombre = ? AND aPaterno = ? AND aMaterno = ?"
+                         		+ "AND correo = ? AND telefono = ? AND direccion =?;";
+
+            prepared = conexion.prepareStatement(buscar);
+
+            /**prepared.setString(1, oCliente.getNombre());
+            prepared.setString(2, oCliente.getPaterno());
+            prepared.setString(3, oCliente.getMaterno());
+            prepared.setString(4, oCliente.getCorreo());
+            prepared.setString(5, oCliente.getTelefono());
+            prepared.setString(6, oCliente.getDireccion());
+*/
+            oResultado = prepared.executeQuery();
+
+            if (!oResultado.next()){
+
+         		insertar =  "INSERT INTO Cliente (nombre, aPaterno, aMaterno, correo, telefono, direccion)"
+         						+ "VALUES (?, ?, ?, ?, ?, ?);";
+
+         		prepared = conexion.prepareStatement(insertar);
+
+         		 /**prepared.setString(1, oCliente.getNombre());
+            	prepared.setString(2, oCliente.getPaterno());
+            	prepared.setString(3, oCliente.getMaterno());
+            	prepared.setString(4, oCliente.getCorreo());
+            	prepared.setString(5, oCliente.getTelefono());
+            	prepared.setString(6, oCliente.getDireccion());
+*/
+            	eExecucion = prepared.executeUpdate();
+
+         		if (eExecucion == 1){
+
+         			JOptionPane.showMessageDialog(null, "Cliente registrado");
+
+         		}
+
+            } else {
+
+            	JOptionPane.showMessageDialog(null, "Error. El cliente ya existe, ingresar datos nuevamente.");
+            }
+
+		} catch(SQLException oExcepcionSQL) {
+
+			JOptionPane.showMessageDialog(null, "Error al establecer la conexi\u00F3n");
+			oExcepcionSQL.printStackTrace();
+
+		} catch(Exception oExcepcion){
+
+			oExcepcion.printStackTrace();
+
+		} finally {
+
+			try{
+
+				if (conexion != null) {
+
+					conexion.close();
+
+				}	
+
+			}catch(SQLException oExcepcion){
+
+					oExcepcion.printStackTrace();
+
+			}
+		}
 	}//fin agregar
+
+
+	public void eliminarCliente(DTOClientes oCliente){
+
+		String eliminar;
+
+		try {
+
+			conexion = getConexion();
+
+			eliminar = "DELETE FROM Clientes WHERE id_cliente = ?;";
+
+			prepared = conexion.prepareStatement(eliminar);
+
+			//prepared.setInt(1, oCliente.getIdCliente());
+
+			if(prepared.executeUpdate() != 0){
+
+				JOptionPane.showMessageDialog(null, "Cliente eliminado");
+			}
+
+			conexion.close();
+		}catch(SQLException oExcepcion){
+
+			oExcepcion.printStackTrace();
+
+		}catch(Exception oExcepcion){
+
+			oExcepcion.printStackTrace();
+
+		}finally{
+
+			try {
+
+				if (conexion != null) {
+
+					conexion.close();
+					
+				}
+			}catch(SQLException oExcepcion){
+
+				oExcepcion.printStackTrace();
+
+			}
+		}
+	}//fin eliminar
+
+	public void modificarCliente(DTOClientes oCliente){
+
+		String modificar;
+
+		try {
+
+			conexion = getConexion();
+
+			modificar = "UPDATE Cliente "
+						+ "SET nombre = ?, aPaterno = ?, aMaterno = ?, correo= ?, correo = ?, telefono = ?, direccion = ?;";
+
+			prepared = conexion.prepareStatement(modificar);
+
+			/**prepared.setString(1, oCliente.getNombre());
+          	prepared.setString(2, oCliente.getPaterno());
+           	prepared.setString(3, oCliente.getMaterno());
+           	prepared.setString(4, oCliente.getCorreo());
+           	prepared.setString(5, oCliente.getTelefono());
+           	prepared.setString(6, oCliente.getDireccion());
+*/
+           	if (prepared.executeUpdate() != 0) {
+           		
+           		JOptionPane.showMessageDialog(null, "Cliente modificado");
+           	
+           	}
+		
+		}catch(SQLException oExcepcion){
+
+			oExcepcion.printStackTrace();
+
+		} catch(Exception oExcepcion){
+
+			oExcepcion.printStackTrace();
+
+		} finally{
+
+			try {
+
+				if (conexion != null) {
+
+					conexion.close();
+					
+				}
+			}catch(SQLException oExcepcion){
+
+				oExcepcion.printStackTrace();
+
+			}
+		}
+
+	}//Fin modificar
+
+	public DTOClientes buscarCliente(int eId){
+
+		String buscar;
+		ResultSet resultado;
+		DTOClientes cliente = new DTOClientes();
+
+
+		return cliente;
+		
+	}//fin buscar
+
+	public void getTabla(DefaultTableModel modelo){
+		
+		String oConsultaTabla;
+		ResultSet oResultado;
+
+		try {
+
+			conexion = getConexion();
+
+			oConsultaTabla = 	  "SELECT id_cliente, nombre, aPaterno, aMaterno, correo, relefono, direccion "
+								+ "FROM Clientes ORDER BY id_cliente;";
+	
+			prepared = conexion.prepareStatement(oConsultaTabla);
+
+			oResultado = prepared.executeQuery();
+
+			modelo.setRowCount(0);
+
+			while (oResultado.next()) {
+				
+				modelo.addRow(new Object[]{oResultado.getInt("id_cliente"), oResultado.getString("nombre"), oResultado.getString("aPaterno"), oResultado.getString("aMaterno"), oResultado.getString("correo"), oResultado.getString("telefono"), oResultado.getString("direccion")});
+
+			} 
+
+			conexion.close();
+
+		} catch (SQLException oExcepcion) {
+
+	        oExcepcion.printStackTrace();
+
+	    } catch (Exception oExcepcion) {
+	         
+	        oExcepcion.printStackTrace();
+
+	    } finally {
+
+	        try {
+
+	            if (conexion != null) {
+
+	               conexion.close();
+	               
+	            } 
+
+	        }catch (SQLException oExcepcion){
+
+	            oExcepcion.printStackTrace();
+
+	        }
+	    }
+	}
+
 
 
 }
